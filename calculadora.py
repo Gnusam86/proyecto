1ee3cc11a60179c7f7bd4c1f55f5bf07b88cc455
SamuelAgama <aza_2099@hotmail.com>
2024-10-12 08:39:13 -0600

    Agregar archivo calculadora con las funciones de suma y resta

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+def suma(a, b):
+    """Esta funcion suma dos numeros"""
+    return a+b
+
+def resta(a, b):
+    """Esta funcion resta dos numeros"""
+    return a-b
+
+if __name__=="__main__":
+    num1= 5
+    num2= 3
+    resultado_suma = suma(num1,num2)
+    resultado_resta = resta(num1,num2)
+
+    print(f"La suma de {num1} y {num2} es:{resultado_suma}")
+    print(f"La resta de {num1} y {num2} es:{resultado_resta}")
